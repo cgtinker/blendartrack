8c6ea6c72f20cc224c75bc46e57db60debc1a5b7
dhsu <d.hsu@online.de>
2020-11-25 22:07:49 +0100

    added event listner

--- EventListner.py
+++ EventListner.py
@@ -16,8 +16,8 @@ importlib.reload(ExecuteModel)
 # ---------------------------------------------------------------------------------------------
 # set path to json file
 
-path = "/Users/Scylla/Downloads/20201113_145709_BS.json"
-# path = "/Users/Scylla/Downloads/FaceMeshData-1.json"
+# path = "/Users/Scylla/Downloads/20201113_145709_BS.json"
+path = "/Users/Scylla/Downloads/FaceMeshData-1.json"
 # path = "/Users/Scylla/Downloads/CameraPose-1.json"
 # ---------------------------------------------------------------------------------------------
 
