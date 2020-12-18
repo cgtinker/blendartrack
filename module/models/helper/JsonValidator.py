a20e3dc9a29f58231e0ba7c0e2bf9db6e076e4c3
dhsu <d.hsu@online.de>
2020-12-18 21:04:00 +0100

    fixed linux import error

--- module/models/helper/JsonValidator.py
+++ module/models/helper/JsonValidator.py
@@ -8,5 +8,6 @@ def validate_json(json_path):
         json_data = json.load(codecs.open(json_path, 'r', 'utf-8-sig'))
     except ValueError as err:
         print('json not valid: ', err)
-        return False
+        return False, ""
+
     return True, json_data
